46e3a90f92764d40b8c0a953cf63e592ba9ed15c
nald-dev <rynvva@gmail.com>
2021-09-05 00:14:03 +0700

    add cache last used simulator order capability

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #! /usr/bin/env node
 
 const commander = require('commander')
+const fs = require('fs-extra')
 
 commander
   .version(require('./package.json').version)
@@ -24,13 +25,32 @@ function start() {
     getIOSDeviceProcess.on('exit', () => {
       const json = JSON.parse(message)
   
-      const choices = []
+      let choices = []
   
       for (const key of Object.keys(json.devices)) {
         if (key.startsWith('com.apple.CoreSimulator.SimRuntime.iOS')) {
-          for (const device of json.devices[key]) {
-            choices.push(device.name)
-          }
+          choices = json.devices[key].map(({ name }) => name)
+        }
+      }
+
+      const userPickHistoryFilePath = __dirname + '/user-pick-history.json'
+
+      let userPickHistory = []
+
+      try {
+        const lastUserPickHistory = require(userPickHistoryFilePath)
+
+        userPickHistory = lastUserPickHistory
+      } catch(err) { }
+      
+      for (let i = userPickHistory.length - 1; i >= 0; i--) {
+        const item = userPickHistory[i]
+        
+        const itemIndex = choices.indexOf(item)
+
+        if (itemIndex != -1) {
+          choices.splice(choices.indexOf(item), 1)
+          choices.unshift(item)
         }
       }
   
@@ -42,7 +62,19 @@ function start() {
           choices
         }
       ])
-      .then(({device}) => spawn('npx', [`react-native`, `run-ios`, `--simulator="${device}"`], {stdio: 'inherit', shell: true}))
+      .then(({device}) => {
+        const deviceIndex = userPickHistory.findIndex(item => item == device)
+
+        if (deviceIndex != -1) {
+          userPickHistory.splice(deviceIndex, 1)
+        }
+
+        userPickHistory.unshift(device)
+
+        fs.writeFileSync(userPickHistoryFilePath, JSON.stringify(userPickHistory, null, 2))
+
+        spawn('npx', [`react-native`, `run-ios`, `--simulator="${device}"`], {stdio: 'inherit', shell: true})
+      })
     })
   } else {
     console.log('Sorry, run-ios is not supported with your operating system')
